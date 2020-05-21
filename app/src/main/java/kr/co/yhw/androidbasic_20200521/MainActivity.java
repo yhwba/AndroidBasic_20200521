d3aa59192fa689326e492ce42442ea2ce5cd3f67
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-21 11:12:45 +0900

    엑티비티 내부에 코드 정리

--- app/src/main/java/kr/co/yhw/androidbasic_20200521/MainActivity.java
+++ app/src/main/java/kr/co/yhw/androidbasic_20200521/MainActivity.java
@@ -10,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+//     이벤트 처리 코드를 모아두는 곳
+    void setupEvents(){
+
+    }
+//    화면에 나와야할 데이터 세팅 코드 모아두는곳
+    void setValues() {
+
     }
 }
---
+++ app/src/main/java/kr/co/yhw/androidbasic_20200521/SecondActivity.java
@@ -0,0 +1,15 @@
+package kr.co.yhw.androidbasic_20200521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
+
